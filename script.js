40d4883786c659f80f8b2e182715e11ace5a62e4
Menhajul Abir <cleverprogrammer404@gmail.com>
2024-10-27 11:00:13 +0600

    Problem-1 Solution
    Problem-1 Solution

---
+++ script.js
@@ -0,0 +1,19 @@
+// Problem-1
+let a = 10;
+let b = 5;
+
+let sum = a + b;
+
+console.log(sum);
+
+let sub = a - b;
+
+console.log(sub);
+
+let multi = a * b;
+
+console.log(multi);
+
+let div = a / b;
+
+console.log(div);
